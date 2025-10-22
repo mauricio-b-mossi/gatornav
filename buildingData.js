cce5365dced05b8dc8f2dd08133f5d4bb08db7bb
mauricio-b-mossi <mauricio.b.mossi@gmail.com>
2025-10-22 08:12:21 -0400

    updated buildingData.js to match LIT room code format 0xxx

--- buildingData.js
+++ buildingData.js
@@ -7,22 +7,22 @@ export const buildingData = {
       1: {
         // Map room numbers to the IDs you will use in your SVG
         rooms: [
-          { number: "101", id: "room-101" },
-          { number: "102", id: "room-102" },
-          { number: "103", id: "room-103" },
-          { number: "104", id: "room-104" },
-          { number: "105", id: "room-105" },
-          { number: "106", id: "room-106" },
-          { number: "107", id: "room-107" },
-          { number: "108", id: "room-108" },
-          { number: "109", id: "room-109" },
-          { number: "110", id: "room-110" },
-          { number: "111", id: "room-111" },
-          { number: "112", id: "room-112" },
-          { number: "113", id: "room-113" },
-          { number: "114", id: "room-114" },
-          { number: "115", id: "room-115" },
-          { number: "116", id: "room-116" },
+          { number: "0101", id: "room-101" },
+          { number: "0102", id: "room-102" },
+          { number: "0103", id: "room-103" },
+          { number: "0104", id: "room-104" },
+          { number: "0105", id: "room-105" },
+          { number: "0106", id: "room-106" },
+          { number: "0107", id: "room-107" },
+          { number: "0108", id: "room-108" },
+          { number: "0109", id: "room-109" },
+          { number: "0110", id: "room-110" },
+          { number: "0111", id: "room-111" },
+          { number: "0112", id: "room-112" },
+          { number: "0113", id: "room-113" },
+          { number: "0114", id: "room-114" },
+          { number: "0115", id: "room-115" },
+          { number: "0116", id: "room-116" },
         ],
 
         // Map entrance names to the entrance IDs you will use in your SVG
@@ -38,22 +38,22 @@ export const buildingData = {
         // NOTE: The 'id' fields MUST match the static IDs in the SVG.
         // The 'number' field provides the dynamic label.
         rooms: [
-          { number: "201", id: "room-101" }, // SVG ID is room-101
-          { number: "202", id: "room-102" }, // SVG ID is room-102
-          { number: "203", id: "room-103" },
-          { number: "204", id: "room-104" },
-          { number: "205", id: "room-105" },
-          { number: "206", id: "room-106" },
-          { number: "207", id: "room-107" },
-          { number: "208", id: "room-108" },
-          { number: "209", id: "room-109" },
-          { number: "210", id: "room-110" },
-          { number: "211", id: "room-111" },
-          { number: "212", id: "room-112" },
-          { number: "213", id: "room-113" },
-          { number: "214", id: "room-114" },
-          { number: "215", id: "room-115" },
-          { number: "216", id: "room-116" }, // SVG ID is room-116
+          { number: "0201", id: "room-101" }, // SVG ID is room-101
+          { number: "0202", id: "room-102" }, // SVG ID is room-102
+          { number: "0203", id: "room-103" },
+          { number: "0204", id: "room-104" },
+          { number: "0205", id: "room-105" },
+          { number: "0206", id: "room-106" },
+          { number: "0207", id: "room-107" },
+          { number: "0208", id: "room-108" },
+          { number: "0209", id: "room-109" },
+          { number: "0210", id: "room-110" },
+          { number: "0211", id: "room-111" },
+          { number: "0212", id: "room-112" },
+          { number: "0213", id: "room-113" },
+          { number: "0214", id: "room-114" },
+          { number: "0215", id: "room-115" },
+          { number: "0216", id: "room-116" }, // SVG ID is room-116
         ],
 
         // Map entrance names to the entrance IDs you will use in your SVG
